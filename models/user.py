f7725412089127d82ca76137542ca15ab52aa6ec
Lilibeth Tabares <1762@holbertonschool.com>
2021-03-11 23:32:44 -0500

    User table

---
+++ models/user.py
@@ -0,0 +1,11 @@
+from config import db
+from models.base_model import Basejson
+
+
+class User(Basejson, db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(45), nullable=False)
+    lastname = db.Column(db.String(45), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    phone = db.Column(db.Integer, nullable=False, unique=False)
+    password = db.Column(db.String(8), nullable=False)
