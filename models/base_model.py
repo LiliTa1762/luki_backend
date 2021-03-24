b03ecdb8e661fc691562e72940135694dff0071d
Lilibeth Tabares <1762@holbertonschool.com>
2021-03-24 15:42:37 -0500

    Adding comments to class and method

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 class Basejson:
+    """ Class to have data in json without a specific parameter """
     def to_dict(self):
+        """ Method to convert into dictionary """
         dictionary: dict = self.__dict__
         dictionary.pop('_sa_instance_state')
         return dictionary
